3a7240d9795bd353f8d36c45d52427a34a051ae2
huzitaiwo <taiwohusayn5@gmail.com>
2021-10-27 02:11:40 +0100

    styled the web page AND made it mobile friendlt

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ const hamburger = document.querySelector(".hamburger");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+
+  //   toggle hamburger and close icon
+  if (navLinks.classList.contains("active")) {
+    hamburger.src = "./images/close.svg";
+  } else {
+    hamburger.src = "./images/hamburger.svg";
+  }
 });
